502219dc122814ff8fef290a8283509aa1039679
Dedivya186 <dedivyayalla24@gmail.com>
2026-07-25 07:17:31 +0530

    task

--- src/com/pack/Main.java
+++ src/com/pack/Main.java
@@ -1,87 +1,128 @@
 package com.pack;
 
+import java.util.ArrayList;
+
 public class Main {
-	   //digit count
-      public static int count_digits(int n) {
-    	  int count=0;
-    	  while(n>0) {
-    		  n/=10;
-    		  count++;
-    	  }
-    	 return count;
-      }
-      //fibonacci
-      public void fibonacci(int n) {
-    	  int f1=0;
-    	  int f2=1;
-    	  while(n>0) {
-    		  System.out.print(f1);
-    		  int a=f1+f2;
-    		  f1=f2;
-    		  f2=a;
-    		 
-    		  n--;
-    	  }
-      }
-      //prime numbers 
-      public void prime_numbers(int start, int end) {
-    	  for (int i = start; i <= end; i++) {
-              if (i < 2) {
-                  continue;
-              }
 
-              boolean isPrime = true;
-              for (int j = 2; j <= i / 2; j++) {
-                  if (i % j == 0) {
-                      isPrime = false;
-                      break;
-                  }
-              }
+    /**
+     * Returns the number of digits in an integer.
+     */
+    public static int count_digits(int n) {
+        n = Math.abs(n);
 
-              if (isPrime) {
-                  System.out.print(i + " ");
-              }
-          }
-          System.out.println();
-    	  
-      }
-      //factors of a number
-      public static void factors(int n) {
-          for (int i = 1; i <= n; i++) {
-              if (n % i == 0) {
-                  System.out.print(i + " ");
-              }
-          }
-          System.out.println();
-      }
-      //GCD
-      public static int gcd(int a, int b) {
-          while (b != 0) {
-              int temp = b;
-              b = a % b;
-              a = temp;
-          }
-          return a;
-      }
+        if (n == 0) {
+            return 1;
+        }
 
-      //  (LCM)
-      public static int lcm(int a, int b) {
-          return (a * b) / gcd(a, b);
-      }
-      public static void main(String[] args) {
-       Main obj=new Main();
-          System.out.println("Digit Count: "+obj.count_digits(1234));
+        int count = 0;
+        while (n > 0) {
+            count++;
+            n /= 10;
+        }
 
-          System.out.print("Fibonacci Series: ");
-          obj.fibonacci(10);
-           System.out.println();
-          System.out.print("Prime Numbers (10 to 50): ");
-          obj.prime_numbers(10, 50);
+        return count;
+    }
 
-          System.out.print("Factors of 24: ");
-          factors(24);
+    /**
+     * Returns the Fibonacci series up to the nth term.
+     */
+    public static ArrayList<Integer> fibonacci(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
 
-          System.out.println("GCD of 24 and 36: " + gcd(24, 36));
-          System.out.println("LCM of 24 and 36: " + lcm(24, 36));
-}
+        int first = 0;
+        int second = 1;
+
+        for (int i = 0; i < n; i++) {
+            list.add(first);
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+
+        return list;
+    }
+
+    /**
+     * Returns all prime numbers in the given range.
+     */
+    public static ArrayList<Integer> prime_numbers(int start, int end) {
+        ArrayList<Integer> primes = new ArrayList<>();
+
+        for (int i = start; i <= end; i++) {
+
+            if (i < 2) {
+                continue;
+            }
+
+            boolean isPrime = true;
+
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                primes.add(i);
+            }
+        }
+
+        return primes;
+    }
+
+    /**
+     * Returns all factors of a number.
+     */
+    public static ArrayList<Integer> factors(int n) {
+        ArrayList<Integer> factorList = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                factorList.add(i);
+            }
+        }
+
+        return factorList;
+    }
+
+    /**
+     * Returns the Greatest Common Divisor (GCD).
+     */
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return Math.abs(a);
+    }
+
+    /**
+     * Returns the Least Common Multiple (LCM).
+     */
+    public static int lcm(int a, int b) {
+        return Math.abs(a * b) / gcd(a, b);
+    }
+
+    /**
+     * Example use cases.
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Digit Count of 12345: " + count_digits(12345));
+
+        System.out.println("Fibonacci Series (10 terms):");
+        System.out.println(fibonacci(10));
+
+        System.out.println("Prime Numbers from 10 to 50:");
+        System.out.println(prime_numbers(10, 50));
+
+        System.out.println("Factors of 24:");
+        System.out.println(factors(24));
+
+        System.out.println("GCD of 24 and 36: " + gcd(24, 36));
+
+        System.out.println("LCM of 24 and 36: " + lcm(24, 36));
+    }
 }
