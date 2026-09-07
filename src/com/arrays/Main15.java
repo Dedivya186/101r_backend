78cbc6744d2bd6278d84e7d25032ad16e9640ade
Dedivya186 <dedivyayalla24@gmail.com>
2026-09-07 13:17:33 +0530

    arrays

--- src/com/arrays/Main15.java
+++ src/com/arrays/Main15.java
@@ -4,23 +4,19 @@ public class Main15 {
 
     public static void main(String[] args) {
 
-        int[] arr = {10, 20, 30, 40, 50};
+        int[] arr = {10, 20, 30, 20, 40, 20};
 
-        int search = 30;
+        int search = 20;
 
-        boolean found = false;
+        int lastIndex = -1;
 
-        for (int num : arr) {
+        for (int i = 0; i < arr.length; i++) {
 
-            if (num == search) {
-                found = true;
-                break;
+            if (arr[i] == search) {
+                lastIndex = i;
             }
         }
 
-        if (found)
-            System.out.println("Element found");
-        else
-            System.out.println("Element not found");
+        System.out.println("Last occurrence = " + lastIndex);
     }
 }
---
+++ src/com/arrays/Main20.java
@@ -0,0 +1,13 @@
+package com.arrays;
+
+public class Main20 {
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        int difference = arr[0] - arr[arr.length - 1];
+
+        System.out.println("Difference = " + difference);
+    }
+}
